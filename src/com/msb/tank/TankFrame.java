3c0ba8c7cbcbe242ccbd6b8b597b48716ffc1370
mashaobing111 <chinamsb@126.com>
2022-10-24 14:56:18 +0800

    在主线程 每隔50ms 刷新窗口，调用repaint 就自动调用paint

--- src/com/msb/tank/TankFrame.java
+++ src/com/msb/tank/TankFrame.java
@@ -37,17 +37,25 @@ public class TankFrame extends Frame {
     //在窗口内画一个黑方块
     @Override
     public void paint(Graphics g) {
+        System.out.println("paint");
+        g.fillRect(x,y,50,50);
 
+        x += 10;
+        //y += 10;
 
     }
     //创建内部类   ：如果只有TankFrame类使用  就创建内部类即可
+    //键盘监听器
     class MyKeyListener extends KeyAdapter{
+        //按下键盘任意按键 调用
         @Override
         public void keyPressed(KeyEvent e) {
             System.out.println("key pressed");
-
+            //x += 10;
+            //repaint();
         }
 
+        //松开键盘任意按键调用
         @Override
         public void keyReleased(KeyEvent e) {
             System.out.println("key released");
