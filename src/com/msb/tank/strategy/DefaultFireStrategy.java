8d4077bf825e3f9b1c32b4e56ade6fe011046e25
mashaobing111 <chinamasb@126.com>
2022-11-05 17:36:59 +0800

    注释装饰器 ，修改为原来代码

--- src/com/msb/tank/strategy/DefaultFireStrategy.java
+++ src/com/msb/tank/strategy/DefaultFireStrategy.java
@@ -20,10 +20,12 @@ public class DefaultFireStrategy implements FireStrategy {
     public void fire(Tank t) {
         int bx = t.getX() + Tank.WIDTH / 2 - Bullet.WIDTH / 2;
         int by = t.getY() + Tank.HEIGHT /2 - Bullet.HEIGHT / 2;
-        GameModel.getInstance().add(
+        //装饰模式
+        /*GameModel.getInstance().add(
 
                         new RectDecorator(
                                 new TailDecorator(
-                                new Bullet(bx, by, t.getDir(),t.getGroup()))));
+                                new Bullet(bx, by, t.getDir(),t.getGroup()))));*/
+        new Bullet(bx, by, t.getDir(), t.getGroup());
     }
 }
